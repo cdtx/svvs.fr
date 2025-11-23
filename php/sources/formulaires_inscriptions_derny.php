c728db85f21fb43ab9bedb2b6b7986a23c3812b0
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2025-11-23 18:03:18 +0100

    Created common file for header

--- php/sources/formulaires_inscriptions_derny.php
+++ php/sources/formulaires_inscriptions_derny.php
@@ -1,26 +1,7 @@
 <!doctype html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<title>SVVS</title>
-
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" href="../css/foundation.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="../css/style.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="../css/responsive.css" type="text/css" media="screen">
-
-	<!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+    <?php include '_head_common.php'?>
 <script language="JavaScript"> 
 
 function valider ( )
--- php/sources/index.php
+++ php/sources/index.php
@@ -1,38 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="'ISO-8859-15">
 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <?php include '_head_common.php'?>
 
-	<title>SVVS</title>
-
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" href="css/foundation.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
-<!-- include jQuery library -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-<!-- include Cycle plugin -->
-<script type="text/javascript" src="js/jquery.cycle.all.latest.js"></script>
-
-	<!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-<?php include 'function.php';?>
-<script type="text/javascript">
-$(document).ready(function() {
-    $('.sponsors').cycle({
-		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-	});
-});
-</script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $('.sponsors').cycle({
+            fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+        });
+    });
+    </script>
 </head>
 <body>
 	<!-- Header -->
