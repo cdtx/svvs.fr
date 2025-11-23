31c595ecb8c9e6df2d06f54564822da9ed492155
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2025-11-23 19:09:51 +0100

    Created common header section

---
+++ php/sources/_header_common.php
@@ -0,0 +1,9 @@
+<div class="inner-header clearfix">
+    <div id="logo" class="left">
+        <h1><a href="/index.php"><img alt="" src="/images/logo.png"></a></h1>
+    </div>
+    <div class="ads-728x90 right">
+        <a href="#"><img alt="" src="/images/logo_2.png"></a>
+    </div>
+</div>
+
--- php/sources/leclub.php
+++ php/sources/leclub.php
@@ -6,15 +6,7 @@
 <body>
 	<!-- Header -->
 	<header class="clearfix">
-		<div class="inner-header clearfix">
-			<div id="logo" class="left">
-				<h1><a href="index.php"><img alt="" src="images/logo.png"></a></h1>
-			</div>
-
-			<div class="ads-728x90 right">
-				<a href="#"><img alt="" src="images/logo_2.png"></a>
-			</div>
-		</div>
+        <?php include('_header_common.php') ?>
 	</header>
 	<!-- End Header -->
 
