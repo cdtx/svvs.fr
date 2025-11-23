2fedfc485bfd6b242a8b434af700f0a81201c45a
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2025-11-23 18:44:17 +0100

    Move navbar to common file

---
+++ php/sources/_nav_common.php
@@ -0,0 +1,35 @@
+<nav id="main-menu" class="left navigation">
+    <ul class="sf-menu no-bullet inline-list m0">
+        <li><a href="/index.php" class="active"><img src="/images/accueil.png" style="width:10px"></a></li>
+        <li><a href="/rando_tdf" >Rando Tour De France 2024</a></li>
+        <li><a href="/actualite">News</a></li>
+        <li><a href="/prochaines-sorties">Les sorties</a></li>
+        <li><a href="/100-pour-sang" >100 pour sang</a></li>
+        <li><a href="/run-bike">le club</a></li>
+        <li><a href="/gentleman-des-champions">Gentleman</a>
+            <ul class="sub-menu">
+                <li><a href="/gentleman-des-champions">Présentation</a></li>
+                <li><a href="/gentleman-des-champions-reglement">Le reglement</a></li>
+                <li><a href="/gentleman-des-champions-parcour">Le Parcours</a></li>
+                <li><a href="/gentleman-des-champions-inscription">Inscription</a></li>
+                <li><a href="/gentleman-des-champions-repas">Repas</a></li>
+                <li><a href="/gentleman-des-champions-resultats">Résultats</a></li>
+                <li><a href="/gentleman-des-champions-presse">La presse</a></li>
+            </ul>
+        </li>
+
+        <li><a href="/marathon-relais">Marathon relais</a>
+            <ul class="sub-menu">
+                <li><a href="/marathon-relais">Présentation</a></li>
+                <li><a href="/marathon-relais-reglement">Le reglement</a></li>
+                <li><a href="/marathon-relais-parcour">Le Parcours</a></li>
+                <li><a href="/marathon-relais-inscription">Inscription</a></li>
+                <li><a href="/marathon-relais-resultats">Résultats</a></li>
+                <li><a href="/marathon-relais-presse">La presse</a></li>
+            </ul>
+        </li>
+        <li><a href="/nos-photos">Galerie</a></li>
+        <li><a href="/nous-contacter">Contact</a></li>
+    </ul>
+</nav>
+
--- php/sources/repas.php
+++ php/sources/repas.php
@@ -21,40 +21,7 @@
 	<!-- Container -->
 	<section class="container row clearfix">
 		<header class="clearfix">
-			<nav id="main-menu" class="left navigation">
-				<ul class="sf-menu no-bullet inline-list m0">
-					<li><a href="index.php" ><img src="images/accueil.png" style="width:10px"></a></li>
-		    		<li><a href="actualite">News</a></li>
-		    		<li><a href="prochaines-sorties">Les sorties</a></li>
-		    		<li><a href="100-pour-sang" >100 pour sang</a></li>
-		    		<li><a href="run-bike">le club</a></li>
-		    		<li><a href="gentleman-des-champions">Gentleman</a>
-						<ul class="sub-menu">
-		    				<li><a href="gentleman-des-champions">Présentation</a></li>
-							<li><a href="gentleman-des-champions-reglement">Le reglement</a></li>
-							<li><a href="gentleman-des-champions-parcour">Le Parcours</a></li>
-							<li><a href="gentleman-des-champions-inscription">Inscription</a></li>
-							<li><a href="gentleman-des-champions-repas">Repas</a></li>
-							<li><a href="gentleman-des-champions-resultats">Résultats</a></li>
-							<li><a href="gentleman-des-champions-presse">La presse</a></li>
-						</ul>
-		    		</li>
-		    		<li><a href="marathon-relais">Marathon relais</a>
-		    			<ul class="sub-menu">
-		    				<li><a href="marathon-relais">Présentation</a></li>
-							<li><a href="marathon-relais-reglement">Le reglement</a></li>
-							<li><a href="marathon-relais-parcour">Le Parcours</a></li>
-							<li><a href="marathon-relais-inscription">Incriptions/départ</a></li>
-							<li><a href="marathon-relais-repas">informations soirée/repas</a></li>
-							<li><a href="marathon-relais-resultats">Résultats</a></li>
-							<li><a href="marathon-relais-presse">La presse</a></li>
-						</ul>
-		    		</li>
-		    		<li><a href="nos-photos">Galerie</a></li>
-		    		<li><a href="nous-contacter">Contact</a></li>
-				</ul>
-			</nav>
-
+            <?php include('_nav_common.php') ?>
 		</header>
 		<!-- Inner Container -->
 		<section class="inner-container clearfix">
